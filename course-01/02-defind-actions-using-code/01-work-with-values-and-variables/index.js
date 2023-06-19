195d6c653fc7f72fa5b423726a0e46891f7b523d
Noey Ignacio <noeyislearning@gmail.com>
2023-06-19 23:56:04 +0800

    Add `01` and `02-01;02` courses

---
+++ course-01/02-defind-actions-using-code/01-work-with-values-and-variables/index.js
@@ -0,0 +1,26 @@
+/* Variables */
+let three = 3; // number
+let name = "Francis"; // string
+let isTrue = true; // boolean
+let empty = null; // null
+let undef = undefined; // undefined
+let obj = {}; // object
+let arr = []; // array
+let symbol = Symbol("symbol"); // symbol
+let float = 3.14; // float
+let nan = NaN; // NaN
+
+/* Artimethic */
+let art_a = 1;
+let art_b = 2;
+let art_c = art_a + art_b; // 3
+
+/* Comparison */
+let com_a = 1;
+let com_b = 2;
+let com_c = com_a > com_b; // false
+
+/* String Concatenation */
+let firstName = "Francis";
+let lastName = "Jones";
+let fullName = firstName + " " + lastName; 
