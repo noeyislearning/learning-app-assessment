96539b2118c699c620821eb372c01556a9dbe9f5
Noey Ignacio <hello@noeyislearning.dev>
2023-06-22 02:46:57 +0800

    build: Add `02-aewqm` module

---
+++ 03-jet/05-dom/02-aewqm/core.js
@@ -0,0 +1,54 @@
+/**
+ * TITLE:
+ * Access Element with querySelector Methods
+ */
+
+// Import Backpack class
+import Backpack from "./js/Backpack.js";
+
+// Create a new object using the Backpack class
+const newBackpack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false,
+  "Jan 15, 2023 15:00:00 PST",
+  "../_assets/img/defbp.svg"
+)
+
+// Template
+const content = 
+`
+  <article class="backpack" id="backpack">
+    <figure class="backpack_image">
+      <img src=${newBackpack.image} alt=""/>
+    </figure>
+    <h1 class="backpack_name">
+      ${newBackpack.name}
+    </h1>
+    <ul class="backpack_features">
+      <li class="packprop backpack_volume">
+        Volume: <span>${newBackpack.volume}l</span>
+      </li>
+      <li class="packprop backpack_color">
+        Color: <span>${newBackpack.color}</span>
+      </li>
+      <li class="packprop backpack_pockets">
+        Number of pockets: <span>${newBackpack.pocketNum}</span>
+      </li>
+      <li class="packprop backpack_strap">
+        Strap length: <span>${newBackpack.strapLength.left} inches (left) / ${newBackpack.strapLength.right} inches (right)</span>
+      </li>
+      <li class="packprop backpack_lid">
+        Lid status: <span>${newBackpack.lidOpen ? "Open" : "Closed"}</span>
+      </li>
+    </ul>
+  </article>
+`
+
+// Access main element
+const main = document.querySelector(".main_content");
+main.innerHTML = content;
