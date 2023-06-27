ee8b2ed745ca7501651c396d8d062d7e51a98109
Noey Ignacio <hello@noeyislearning.dev>
2023-06-28 01:41:06 +0800

    refactor: Fix some bug in `feedbacks` logic

--- 08-building-a-website-with-nodejs-and-expressjs-3/src/routes/feedback.js
+++ 08-building-a-website-with-nodejs-and-expressjs-3/src/routes/feedback.js
@@ -1,18 +1,49 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
+
+const validations = [
+  check("name")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("A name is required"),
+  check("email")
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("A valid email address is required"),
+  check("title")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("A title is required"),
+  check("message")
+    .trim()
+    .isLength({ min: 5 })
+    .escape()
+    .withMessage("A message is required"),
+];
+
 
 module.exports = ( { feedbackService } ) => {
   
     router.get("/", async (req, res, next) => {
       try {
         const feedback = await feedbackService.getList();
+        const errors = req.session.feedback ? req.session.feedback.errors : false;
+        const successMessage = req.session.feedback ? req.session.feedback.message : false;
+        req.session.feedback = {};
+
         res.render(
           "layout",
           {
             pageTitle: "Feedback",
             template: "feedback",
-            feedback
+            feedback,
+            errors,
+            successMessage
           }
         )
       }
@@ -21,9 +52,32 @@ module.exports = ( { feedbackService } ) => {
       }
     });
 
-  router.post("/", (req, res) => {
-    console.log(req.body)
-    return res.send(`Feedback form posted.`)
+  router.post("/", validations, async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if(!errors.isEmpty()) {
+        req.session.feedback = {
+          errors: errors.array(),
+        };
+        return res.redirect("/feedback");
+      }
+
+      const { name, email, title, message } = req.body;
+      await feedbackService.addEntry(
+        name, 
+        email,
+        title,
+        message
+      );
+      req.session.feedback = {
+        message: "Thank you for your feedback!",
+      }
+      return res.redirect('/feedback');
+    }
+    catch(err) {
+      return next(err);
+    }
   });
 
   return router;
