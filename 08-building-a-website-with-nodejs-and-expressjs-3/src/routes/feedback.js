a208a68123baad508037512904f845fbefaa2ad8
Noey Ignacio <hello@noeyislearning.dev>
2023-06-28 01:05:40 +0800

    refactor: Update logic in `feedbacks`

--- 08-building-a-website-with-nodejs-and-expressjs-3/src/routes/feedback.js
+++ 08-building-a-website-with-nodejs-and-expressjs-3/src/routes/feedback.js
@@ -3,12 +3,26 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = ( { feedbackService } ) => {
-  router.get("/", async (req, res) => {
-    const feedback = await feedbackService.getList();
-    return res.json(feedback);
-  });
+  
+    router.get("/", async (req, res, next) => {
+      try {
+        const feedback = await feedbackService.getList();
+        res.render(
+          "layout",
+          {
+            pageTitle: "Feedback",
+            template: "feedback",
+            feedback
+          }
+        )
+      }
+      catch(err) {
+        return next(err);
+      }
+    });
 
   router.post("/", (req, res) => {
+    console.log(req.body)
     return res.send(`Feedback form posted.`)
   });
 
