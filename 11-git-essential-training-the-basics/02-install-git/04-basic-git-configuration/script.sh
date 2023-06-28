c30b59417c52fe18db4ff8765a848c21fa803430
Noey Ignacio <hello@noeyislearning.dev>
2023-06-29 02:12:14 +0800

    build: Add `02-04` module

---
+++ 11-git-essential-training-the-basics/02-install-git/04-basic-git-configuration/script.sh
@@ -0,0 +1,23 @@
+# 1. Set the user name
+git config --global user.name "Francis Ignacio"
+
+# 2. Set the user email
+git config --global user.email "example@example.com"
+
+# 3. Set the default editor
+git config --global core.editor "code --wait"
+
+# 4. Check the configuration
+git config --list
+
+# 5. Get help
+git help config
+
+# 6. Get help for a specific command
+git config --help
+
+# 7. Get version
+git --version
+
+# 8. Change color
+git config --global color.ui true
