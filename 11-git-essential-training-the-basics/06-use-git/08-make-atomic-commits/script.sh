59b3661e59d6c6ed413c8cbb733ff028e2dfeacf
Noey Ignacio <hello@noeyislearning.dev>
2023-06-29 03:16:54 +0800

    build: Add `06-08` module

---
+++ 11-git-essential-training-the-basics/06-use-git/08-make-atomic-commits/script.sh
@@ -0,0 +1,6 @@
+# Make atomic commits
+
+# - Small commits
+# - Only affect a single aspect
+# - Easy to understand, to revert, to cherry-pick, work with, and to find bugs
+# - Improves collaboration
